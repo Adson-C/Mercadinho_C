23cbb711f0f0197aa23922a35ba69c9faad2e53b
Adson Conceição <adsonconcecao@yahoo.com.br>
2024-06-09 18:05:15 -0300

    implemtando função InfoProduto/Menu

--- mercado.c
+++ mercado.c
@@ -19,6 +19,7 @@ void menu();
 void infoProduto(Produto prod);
 void cadastrarProduto();
 void listarProdutos();
+void comprarProdutos();
 void visualizarCarrinho();
 Produto pegarProdutoPorCodigo(int codigo);
 int * temNoCarrinho(int codigo);
@@ -37,13 +38,57 @@ int main()
 }
 
 void infoProduto(Produto prod){
-    // falta implementar
+    printf("Código: %d \nNome: %s \nPreço: %.2f\n", prod.codigo, strtok(prod.nome, "\n"), prod.preco);
+}
+
+void menu(){
+    printf("============================================\n");
+    printf("==========>- Seja Bem-Vindo(a) -<============\n");
+    printf("==========>- Adson - Mercado -<===============\n");
+    printf("===============================================\n");
+
+    printf("Selecione uma opção abaixo:\n");
+    printf("1 - Cadastrar um novo produto\n");
+    printf("2 - Listar todos os produtos\n");
+    printf("3 - Comprar produto\n");
+    printf("4 - Visualizar carrinho\n");
+    printf("5 - Fechar pedido\n");
+    printf("6 - Sair\n");
+    printf("===============================================\n");
+
+    int opcao;
+    scanf("%d", &opcao);
+    getchar();
+
+    switch(opcao){
+        case 1: cadastrarProduto();
+        break;
+        case 2: listarProdutos();
+        break;
+        case 3: comprarProdutos();
+        break;
+        case 4: visualizarCarrinho();
+        break;
+        case 5: fecharPedido();
+        break;
+        case 6:
+            printf("Volte sempre...\n");
+            exit(0);
+        default: 
+            printf("Opção inválida!\n");
+            Sleep(2);
+            menu();
+        break;
+    }
 }
 
 void cadastrarProduto(){
     // falta implementar
 }
 void listarProdutos(){
+    // falta implementar
+}
+void comprarProdutos(){
     // falta implementar
 }
 void visualizarCarrinho(){
